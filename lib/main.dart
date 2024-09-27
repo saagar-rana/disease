58a4f8d3d2ad2a1920fe5b0d28c43a94049ef6ab
sagar <sagarranabhat757@gmail.com>
2024-09-27 13:58:41 +0545

    classify

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pytorch_lite/pytorch_lite.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +35,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Hme Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -55,18 +59,54 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+  ClassificationModel? _imageModel;
+  String? _imagePrediction;
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel();
+  }
+
+Future<void> loadModel() async {
+  String pathImageModel = "assets/model.pt";
+  try {
+    _imageModel = await PytorchLite.loadClassificationModel(
+        pathImageModel, 224, 224,
+        labelPath: "assets/labels_classification.txt");
+        print('donme');
+  } on PlatformException {
+      print("only supported for android");
+    }
+}
+
+  Future getClassify() async{
+    
+    // Load the image as bytes (example from assets)
+  ByteData byteData = await rootBundle.load(r'assets\images\digital_camera_photo-1080x675.jpg');
+  Uint8List imageBytes = byteData.buffer.asUint8List();
+
+  // Assuming _imageModel is your loaded PyTorch model
+  String? result;
+  try {
+    // Call the getImagePrediction method
+    result = await _imageModel!.getImagePrediction(
+      imageBytes,
+    );
+    print("Prediction Result: $result");
+  } catch (e) {
+    print("Error during image prediction: $e");
+  }
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _imagePrediction = result;
+    
     });
   }
+
+
+  
 
   @override
   Widget build(BuildContext context) {
@@ -106,17 +146,17 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many ties:',
+              'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              '$_imagePrediction',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: getClassify,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
